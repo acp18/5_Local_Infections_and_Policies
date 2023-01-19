e12f9309792d4a8d0b4e673bfebbe44a98782d0c
Foundry (vector-client-token) <>
2023-01-19 17:29:26 +0000

    Update workbook

--- pipeline.R
+++ pipeline.R
@@ -1,2 +1,10 @@
 
 
+@transform_pandas(
+    Output(rid="ri.vector.main.execute.f8b53b2e-113c-4319-b65d-b0ff8f035ff6"),
+    county_daily_ma=Input(rid="ri.foundry.main.dataset.11d706f0-587c-40ed-a9da-601a12b3b4bd")
+)
+unnamed <- function(county_daily_ma) {
+    
+}
+

e03ff043ffd8d14d09837c0ea3dae03e46febddc
Foundry (vector-client-token) <>
2023-01-19 17:29:47 +0000

    Update workbook

--- pipeline.R
+++ pipeline.R
@@ -1,10 +1,19 @@
 
 
 @transform_pandas(
-    Output(rid="ri.vector.main.execute.f8b53b2e-113c-4319-b65d-b0ff8f035ff6"),
+    Output(rid="ri.foundry.main.dataset.27a0e619-0e4a-4dd8-b06f-6d7e763da771"),
     county_daily_ma=Input(rid="ri.foundry.main.dataset.11d706f0-587c-40ed-a9da-601a12b3b4bd")
 )
-unnamed <- function(county_daily_ma) {
-    
+date_first_case <- function(county_daily_ma) {
+    ##Subset dataset to only those counties with positive case counts and compute the minimum date per county
+    df<-subset(county_daily_ma,covid19_total_cases>0)
+     df1<-df %>% 
+    group_by(local_code) %>% 
+    mutate(first_case_date = min(date, na.rm = T))
+    df2<-df1[,c("local_code","date","first_case_date")]
+    ##Merge back into full dataset
+    df3<-merge(county_daily_ma,df2,by=c("local_code","date"))
+
+    return(df3)
 }
 

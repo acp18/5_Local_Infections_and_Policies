600053b7a580e528c0b482f2b0f8d43b32131949
Foundry (vector-client-token) <>
2023-01-19 18:59:24 +0000

    Update workbook

--- pipeline.R
+++ pipeline.R
@@ -14,13 +14,13 @@ date_first_case <- function(county_daily_ma) {
     ##Merge back into full dataset
     df3<-merge(county_daily_ma,df2,by=c("local_code","date"))
 
- df4<-df3 %>% 
+ df4<-df %>% 
     group_by(CBSA_CODE) %>% 
     mutate(first_case_date_cbsa = min(date, na.rm = T))
-    df2<-df1[,c("local_code","date","first_case_date_cbsa")]
+    df5<-df1[,c("local_code","date","first_case_date_cbsa")]
     ##Merge back into full dataset
-    df5<-merge(df3,df4,by=c("local_code","date"))
+    df6<-merge(df3,df5,by=c("local_code","date"))
 
-    return(df5)
+    return(df6)
 }
 

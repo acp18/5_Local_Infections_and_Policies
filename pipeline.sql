c48996472f136d429bf666ac78e6434c0a8ed098
Foundry (vector-client-token) <>
2023-01-23 17:32:46 +0000

    Update workbook

--- pipeline.sql
+++ pipeline.sql
@@ -17,7 +17,7 @@ FROM county_daily_w_cbsa
     BU_SC_SDOH_N3C_2018_20201130=Input(rid="ri.foundry.main.dataset.f9fb2781-bed3-421e-bb57-6eaa24ddd85d"),
     county_daily_status=Input(rid="ri.foundry.main.dataset.99814779-5924-4e29-8df1-5d80b4da5cf7")
 )
-SELECT a.*, b.CBSA_CODE, b.CBSA_Name
+SELECT a.*, CASE WHEN covid19_new_cases<0 then NULL else covid19_new_cases end as drvd_new_cases, b.CBSA_CODE, b.CBSA_Name
 FROM county_daily_status a LEFT JOIN BU_SC_SDOH_N3C_2018_20201130 b
 ON a.local_code = b.FIPS_CODE
 
@@ -25,6 +25,6 @@ ON a.local_code = b.FIPS_CODE
     Output(rid="ri.foundry.main.dataset.0487bd03-e1d9-47f3-be58-dfda80d883e1"),
     date_first_case=Input(rid="ri.foundry.main.dataset.27a0e619-0e4a-4dd8-b06f-6d7e763da771")
 )
-SELECT *, DATEDIFF(date , first_case_date_county) as days_from_1st_case_county, DATEDIFF(day, date , first_case_date_cbsa) as days_from_1st_case_cbsa
+SELECT *, DATEDIFF(date , first_case_date_county) as days_from_1st_case_county, DATEDIFF( date , first_case_date_cbsa) as days_from_1st_case_cbsa
 FROM date_first_case
 

3dd8fbc6e5105420434de9b9be8cfd3f5272e8f7
Foundry (vector-client-token) <>
2023-08-21 15:50:56 +0000

    Update workbook

--- pipeline.R
+++ pipeline.R
@@ -4,6 +4,18 @@ library(dplyr)
     Output(rid="ri.foundry.main.dataset.27a0e619-0e4a-4dd8-b06f-6d7e763da771"),
     county_daily_ma=Input(rid="ri.foundry.main.dataset.11d706f0-587c-40ed-a9da-601a12b3b4bd")
 )
+#==========================================================================
+# Author:         Alyssa Platt (alyssa.platt@duke.edu)
+# Project:        N3C Case-Crossover study
+# Created:        
+# Last modified:  
+# Purpose:        Compute date of first COVID-19 case in county
+
+# Inputs: 'county_daily_ma'
+# Output: 'date_first_case'
+                
+#==============================================================================*/
+
 date_first_case <- function(county_daily_ma) {
     ##Subset dataset to only those counties with positive case counts and compute the minimum date per county
     df<-subset(county_daily_ma,covid19_total_cases>0)
